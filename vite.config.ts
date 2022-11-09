112a70cc1db41eb61e00a4313b70f953f048a2e4
Aiwaka <Kw.I.KU.1414@gmail.com>
2022-11-10 02:04:00 +0900

    enable to resolve alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { sveltekit } from "@sveltejs/kit/vite";
+import { fileURLToPath } from "node:url";
 import type { UserConfig } from "vite";
 
 const config: UserConfig = {
   server: {
     port: 8080
+  },
+  // @でsrcを参照できるようにしている. tsconfigでもpathを設定する
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
   },
   plugins: [sveltekit()]
 };
