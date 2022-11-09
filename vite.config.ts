4cb16ec87da06aa190088988da10d55cf84eafa2
Aiwaka <Kw.I.KU.1414@gmail.com>
2022-11-09 16:14:27 +0900

    change port and add assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import type { UserConfig } from "vite";
 
 const config: UserConfig = {
+  server: {
+    port: 8080
+  },
   plugins: [sveltekit()]
 };
 
