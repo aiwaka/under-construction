aedaf4c7dab20d985b3b55fa699b6a5b1874976a
Aiwaka <Kw.I.KU.1414@gmail.com>
2022-11-10 16:45:53 +0900

    add profile sec

---
+++ src/ProfileSection.svelte
@@ -0,0 +1,23 @@
+<section id="profile">
+  <h2>Aiwaka</h2>
+  <img class="my-icon" src="my-icon.png" alt="aiwaka's icon" />
+  <ul>
+    <li>Location: Japan, Kumamoto and Nara</li>
+    <li>Education: Graduate School of Informatics, Kyoto University</li>
+    <li />
+  </ul>
+  <p>遊ぶことが好きです。</p>
+
+  <p>
+    趣味はビデオゲーム、作曲、ピアノ、イラスト、カラオケ、ドライブ、プログラミングです。今後も増えます。
+  </p>
+  <p>小学生まで熊本に住み、中学受験を経て奈良に移りました。京都大学に奈良から通っています。</p>
+  <p />
+</section>
+
+<style>
+  .my-icon {
+    border-radius: 10rem;
+    overflow: hidden;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,9 @@
 <script>
+  import ProfileSection from "@/ProfileSection.svelte";
   import TopImage from "@/TopImage.svelte";
 </script>
 
 <main>
   <TopImage />
-  <section id="profile">
-    <h2>Aiwaka</h2>
-    <p>Japan, Nara</p>
-  </section>
+  <ProfileSection />
 </main>
