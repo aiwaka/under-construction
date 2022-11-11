8ef3549196c46cc1d80d6872a1a46d6a48ab910f
Aiwaka <Kw.I.KU.1414@gmail.com>
2022-11-12 03:36:18 +0900

    change contact design

--- src/sections/ContactSection.svelte
+++ src/sections/ContactSection.svelte
@@ -5,6 +5,30 @@
 <section class="contact-section section-item">
   <h2 id="contact">Contact</h2>
   <span>お問い合わせはメールにてお願いいたします。</span>
+  <table class="contact-table">
+    <tbody>
+      <tr>
+        <th>Mail</th>
+        <td>
+          <a
+            href="mailto:Kw.I.KU.2525@gmail.com?subject=%E3%81%8A%E5%95%8F%E3%81%84%E5%90%88%E3%82%8F%E3%81%9B（From Under-Construction）"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Kw.I.KU.2525@gmail.com
+          </a>
+        </td>
+      </tr>
+      <tr>
+        <th>GitHub</th>
+        <td>
+          <a href="https://github.com/littleIkawa" target="_blank" rel="noopener noreferrer">
+            @littleIkawa
+          </a>
+        </td>
+      </tr>
+    </tbody>
+  </table>
   <div class="contact-container">
     <a
       href="mailto:Kw.I.KU.2525@gmail.com?subject=%E3%81%8A%E5%95%8F%E3%81%84%E5%90%88%E3%82%8F%E3%81%9B（From Under-Construction）"
@@ -13,22 +37,45 @@
     >
       <div class="contact-item mail">
         <ServiceIcon name="gmail" size="large" />
-        <span>Kw.I.KU.2525@gmail.com</span>
+        <span>Mail</span>
       </div>
     </a>
     <a href="https://github.com/littleIkawa" target="_blank" rel="noopener noreferrer">
       <div class="contact-item github">
         <ServiceIcon name="github" size="large" />
-        <span>@littleIkawa</span>
+        <span>GitHub</span>
       </div>
     </a>
   </div>
 </section>
 
 <style>
+  .contact-table {
+    border-collapse: collapse;
+    margin: 1rem 2rem;
+  }
+  .contact-table tr {
+    border-top: 1px solid darkslateblue;
+    border-bottom: 1px solid darkslateblue;
+  }
+  .contact-table th,
+  td {
+    text-align: left;
+    padding: 1rem 2rem;
+  }
+  @media (max-width: 1024px) {
+    .contact-table {
+      margin: 1rem auto;
+    }
+    .contact-table th,
+    td {
+      padding: 1rem 0.4rem;
+    }
+  }
+
   .contact-container {
     display: flex;
-    margin: 12rem auto;
+    margin: 7rem auto;
     justify-content: center;
   }
   .contact-item {
