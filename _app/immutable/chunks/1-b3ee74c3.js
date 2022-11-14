5403a99fb0ef10e0e7f8846f0cd233d81d928558
littleIkawa <littleIkawa@users.noreply.github.com>
2022-11-14 15:08:55 +0000

    deploy: b6228e87f2eab19fe1c248cb712fa304b662f25d

--- _app/immutable/chunks/1-b3ee74c3.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-c0e47273.js";export{t as component};
---
+++ _app/immutable/chunks/2-fb5a138c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-8573b419.js";export{t as component};
