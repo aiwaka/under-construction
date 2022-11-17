a8573f1db4a7bd3deeb43474a40848d68313f5e9
Aiwaka <Kw.I.KU.1414@gmail.com>
2022-11-17 10:24:49 +0900

    add base to main css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
+<script lang="ts">
+  import { base } from "$app/paths";
+
+  const cssPath = `${base}/main.css`;
+</script>
+
 <!-- layoutを使っているが現状意味があまりない. 複数ページになったときのため残す -->
 <svelte:head>
-  <link rel="stylesheet" href="main.css" />
+  <link rel="stylesheet" href={cssPath} />
 </svelte:head>
 
 <slot />
