9bf34f299bf600cb5f936686e9ea01326c495b0b
Aiwaka <Kw.I.KU.1414@gmail.com>
2022-11-17 18:07:35 +0900

    add article card component

--- src/components/blog/ArticleCard.svelte
+++ src/components/blog/ArticleCard.svelte
@@ -1,42 +1,65 @@
 <script lang="ts">
   import { base } from "$app/paths";
+  import ArticleTag from "./ArticleTag.svelte";
 
+  export let id: string;
   export let title: string;
   export let thumbnailFilename: string;
   export let date: Date;
   export let overview: string;
-  export let tag: string[];
+  export let tagList: string[];
 
   const thumbnailPath = `${base}/blog/thumb/${thumbnailFilename}`;
+
+  const dateText = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
 </script>
 
-<div class="article-card">
-  <div class="img-container">
-    <img src={thumbnailPath} alt="thumbnail" />
+<a href={`/blog/${id}`}>
+  <div class="article-card">
+    <div class="img-container">
+      <img src={thumbnailPath} alt="thumbnail" />
+    </div>
+    <div class="card-contents">
+      <h2>{title}</h2>
+      <span>{dateText}</span>
+      <p>{overview}</p>
+      <div class="tag-container">
+        {#each tagList as tag}
+          <ArticleTag {tag} />
+        {/each}
+      </div>
+    </div>
   </div>
-  <div class="card-contents">
-    <h2>{title}</h2>
-    <span>{date.toString()}</span>
-    <p>{overview}</p>
-    {#each tag as item}
-      {`#${item}`}
-    {/each}
-  </div>
-</div>
+</a>
 
 <style>
   .article-card {
+    border: 1px solid darkslateblue;
+    border-radius: 5px;
+    background-color: rgba(240, 255, 255, 0.6);
     position: relative;
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
+    width: 30%;
+  }
+  .article-card:hover {
+    background-color: darkslateblue;
+    color: azure;
   }
   .img-container {
-    width: 20%;
-    height: 20%;
+    border: 4px solid darkslateblue;
+    width: 100%;
+    height: 50%;
   }
   .img-container > img {
     width: 100%;
     height: 100%;
-    clip-path: path("M 0 200 L 0,75 A 5,5 0,0,1 150,75 L 200 200 z");
+  }
+  .card-contents {
+    padding: 1rem 2rem;
+  }
+  .tag-container {
+    display: flex;
+    flex-wrap: wrap;
   }
 </style>
---
+++ src/components/blog/ArticleTag.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  export let tag: string;
+</script>
+
+<div class="article-tag">
+  #{tag}
+</div>
+
+<style>
+  .article-tag {
+    background-color: slateblue;
+    border: 3px solid darkslateblue;
+    border-radius: 4px;
+    color: azure;
+    width: fit-content;
+    padding: 0.3rem 0.8rem;
+    margin: 0 0.3rem;
+  }
+  .article-tag:first-child {
+    margin-left: 0;
+  }
+</style>
