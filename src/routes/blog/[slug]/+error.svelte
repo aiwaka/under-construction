a9e527708a402ad2f9718bdcc2e9b9efa22264d4
Aiwaka <Kw.I.KU.1414@gmail.com>
2022-11-20 18:47:25 +0900

    add 404 page

---
+++ src/routes/blog/[slug]/+error.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { base } from "$app/paths";
+
+  const bgCssPath = `${base}/style/bg2.css`;
+</script>
+
+<svelte:head>
+  <title>Not Found</title>
+  <link rel="stylesheet" href={bgCssPath} />
+</svelte:head>
+
+<div class="header-margin" />
+<main class="main-container">
+  <div>
+    <h1>ページが見つかりませんでした。</h1>
+  </div>
+</main>
+
+<style>
+  .header-margin {
+    height: 10rem;
+  }
+  .main-container {
+    width: 70%;
+    margin: auto;
+  }
+  @media (max-width: 1024px) {
+    .main-container {
+      width: 95%;
+    }
+  }
+</style>
