67d66c7db315a24583b74720b43c1f1f3918fd06
Aiwaka <Kw.I.KU.1414@gmail.com>
2022-11-22 17:10:29 +0900

    using api endpoint

--- src/lib/articles/index.ts
+++ src/lib/articles/index.ts
@@ -1,3 +1,29 @@
+export class ArticleAttributeJson {
+  id: string;
+  title: string;
+  description: string;
+  // ファイル名を指定
+  thumbnail: string;
+  date: string;
+  tags: string[];
+
+  constructor(
+    id: string,
+    title: string,
+    description: string,
+    thumbnail: string,
+    date: string,
+    tags: string[]
+  ) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.thumbnail = thumbnail;
+    this.date = date;
+    this.tags = tags;
+  }
+}
+
 export class ArticleAttribute {
   id: string;
   title: string;
@@ -21,6 +47,21 @@ export class ArticleAttribute {
     this.thumbnail = thumbnail;
     this.date = date;
     this.tags = tags;
+  }
+
+  // コンストラクタのオーバーロードはしたくないのでこのようにstatic methodで記述する
+  /**
+   * json形式のArticleAttributeJsonの日付をDateオブジェクトに変換してArticleAttributeを作成する
+   */
+  static fromJson(obj: ArticleAttributeJson) {
+    return new ArticleAttribute(
+      obj.id,
+      obj.title,
+      obj.description,
+      obj.thumbnail,
+      new Date(obj.date),
+      obj.tags
+    );
   }
 }
 
