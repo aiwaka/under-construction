2d9715aad379c9c312c5d9a10cb33ed59a7af352
Aiwaka <Kw.I.KU.1414@gmail.com>
2023-03-06 02:05:08 +0900

    add integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [svelte(), tailwind(), image(), mdx()]
+});
