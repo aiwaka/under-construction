c95bf9e995dfd35d5083cc0088b11cfb18eedbb0
Aiwaka <Kw.I.KU.1414@gmail.com>
2023-03-06 21:59:45 +0900

    split component functionality

--- src/components/IndexBar.svelte
+++ src/components/IndexBar.svelte
@@ -1,42 +1,52 @@
 <script lang="ts">
   import PageLink from "./PageLink.svelte";
+  import AnimatedLink from "./utils/AnimatedLink.svelte";
 
   interface IndexData {
     name: string;
     href: string;
   }
-  export let currentRoute: string;
   export let indexList: IndexData[];
 </script>
 
 <div class="contents-link-wrapper">
   {#each indexList as idx}
     {@const href = idx.href}
-    <PageLink {currentRoute} {href} height={"3rem"}>{idx.name}</PageLink>
+    <div class="index-link">
+      <AnimatedLink {href}>
+        {idx.name}
+      </AnimatedLink>
+    </div>
   {/each}
 </div>
 
-<style>
+<style lang="postcss">
   .contents-link-wrapper {
     display: flex;
     justify-content: space-evenly;
-    margin: 4rem 1.4rem;
+    margin: 3.2rem 1rem;
     background-image: -webkit-linear-gradient(
       left,
       transparent,
-      rgba(72, 61, 139, 0.7) 8%,
-      rgba(72, 61, 139, 0.7) 92%,
+      rgba(var(--main-color-params), 0.7) 5%,
+      rgba(var(--main-color-params), 0.7) 95%,
       transparent
     );
     background-position: center;
     background-repeat: no-repeat;
+    height: 2.2rem;
+    line-height: 2.2rem;
   }
   @media (max-width: 1024px) {
     .contents-link-wrapper {
       margin: 2rem 0;
       position: relative;
       bottom: 0;
-      height: 3rem;
+      height: 1.6rem;
+      line-height: 1.6rem;
     }
+  }
+  .index-link {
+    height: 2.2rem;
   }
 </style>
