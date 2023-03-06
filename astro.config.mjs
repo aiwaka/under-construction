9f7ad4ff8f353b12ec8dc9be5b1e192b26d76242
Aiwaka <Kw.I.KU.1414@gmail.com>
2023-03-06 11:10:45 +0900

    remove tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import svelte from "@astrojs/svelte";
-import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind(), image(), mdx()]
+  integrations: [svelte(), image(), mdx()],
+
 });
