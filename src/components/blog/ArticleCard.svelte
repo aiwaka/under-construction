7d025f4e63005935159158065a5d9f2bd6ad6a1e
Aiwaka <Kw.I.KU.1414@gmail.com>
2023-03-08 13:38:49 +0900

    copy files (remains bug in article list page)

---
+++ src/components/blog/ArticleCard.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+  import type { ArticleAttribute } from "@lib/articles";
+  import ArticleTag from "./ArticleTag.svelte";
+
+  export let metadata: ArticleAttribute;
+
+  const { id, title, thumbnail, date, tags } = metadata;
+
+  const thumbnailPath = `/blog/thumb/${thumbnail}`;
+
+  const dateText = `${date.getFullYear()}-${
+    date.getMonth() + 1
+  }-${date.getDate()}`;
+</script>
+
+<a class="article-link" href={`/blog/${id}`}>
+  <div class="article-card">
+    <div class="img-container">
+      <img src={thumbnailPath} alt="thumbnail" />
+    </div>
+    <div class="card-contents">
+      <h2>{title}</h2>
+      <span>{dateText}</span>
+      <div class="tag-container">
+        {#each tags as tag}
+          <ArticleTag {tag} />
+        {/each}
+      </div>
+    </div>
+  </div>
+</a>
+
+<style>
+  /* このリンクの親はグリッドボックスである前提なので, その大きさ指定やgap等の影響を受ける. */
+  .article-link {
+    display: block;
+    width: 100%;
+    border: 1px solid var(--main-color);
+    border-radius: 3px;
+    background-color: rgba(var(--main-bg-params), 0.6);
+    overflow: hidden;
+    margin: 1.4rem 0;
+  }
+  .article-card {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+  }
+  .article-card:hover {
+    background-color: var(--main-color);
+    color: var(--main-bg-color);
+  }
+  .img-container {
+    border: 4px solid var(--main-color);
+    width: 100%;
+    height: 12rem;
+    clip: rect(0, 0, 0, 0);
+  }
+  .img-container > img {
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+  }
+  .card-contents {
+    padding: 0.5rem 1rem;
+  }
+  .tag-container {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0.7rem auto;
+  }
+</style>
