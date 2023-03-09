83752a9ab5b4c94f929da97617dd9f8f00aea6c9
Aiwaka <Kw.I.KU.1414@gmail.com>
2023-03-09 19:26:04 +0900

    isolate functions

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,3 +1,6 @@
+import { ArticleAttribute } from "@lib/articles";
+import type { AstroGlobal, MDXInstance } from "astro";
+
 const getFilenameFromPath = (path: string): string | null => {
   const matched = path.match(".+/(.+?).[a-z]+([?#;].*)?$");
   return matched ? matched[1] : null;
@@ -21,6 +24,49 @@ const thumbnailPath = (filename: string): string => {
 /**
  * 1ページあたりの記事数
  */
-const POST_PER_PAGE = 9;
+const POST_PER_PAGE = 1 as const satisfies number;
 
-export { getFilenameFromPath, dateText, thumbnailPath, POST_PER_PAGE };
+interface FetchPostsOptions {
+  tag?: string;
+}
+/**
+ * mdxが保存されている箇所へのパスを指定し, draftフラグがついたものを除き, 各ページの情報をまとめたリストを返す. オプションでタグによるフィルタリングができる.
+ * @param posts Astro.globで取得したMDXオブジェクトのリスト.
+ * @param options
+ */
+const getAttrList = (
+  posts: MDXInstance<Record<string, any>>[],
+  options: FetchPostsOptions = {}
+) => {
+  // draftがtrueのものを除く
+  const nonDraftPosts = posts.filter((post) => !post.frontmatter.draft);
+  // タグがある場合はそれを含むもののみ取得し, 指定がない場合はなにもしない.
+  const filteredPosts = nonDraftPosts.filter((post) =>
+    options.tag ? post.frontmatter.tags.includes(options.tag) : true
+  );
+  // mdxオブジェクトのpostリストを自分のデータ形式に変換する
+  const attrList = filteredPosts.map((post) => {
+    const frontmatter = post.frontmatter as Record<string, any> &
+      ArticleAttribute;
+    const filename = getFilenameFromPath(post.file) ?? "";
+    return new ArticleAttribute(
+      filename,
+      frontmatter.title,
+      frontmatter.description,
+      frontmatter.thumbnail,
+      new Date(frontmatter.date),
+      frontmatter.tags
+    );
+  });
+  // ソート
+  attrList.sort((a, b) => (a.date < b.date ? 1 : -1));
+  return attrList;
+};
+
+export {
+  getFilenameFromPath,
+  dateText,
+  thumbnailPath,
+  getAttrList,
+  POST_PER_PAGE,
+};
