4bdec8c60330274991e972dae5468e2342c2de99
Aiwaka <Kw.I.KU.1414@gmail.com>
2023-03-22 20:32:10 +0900

    complete todos

--- src/lib/articles/index.ts
+++ src/lib/articles/index.ts
@@ -57,3 +57,14 @@ export class ArticleAttribute {
 //     this.content = content;
 //   }
 // }
+
+/**
+ * `alt`, `caption`は両方省略はできず, この場合警告がコンソールに表示される.
+ * `alt`を省略した場合自動的に`caption`が用いられる.
+ */
+export interface BlogImageProps {
+  src: ImageMetadata | Promise<{ default: ImageMetadata }>;
+  width: number;
+  alt?: string;
+  caption?: string;
+}
