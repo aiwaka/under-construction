d68aa476dc541bc978141e8690cc845fa783e294
Aiwaka <Kw.I.KU.1414@gmail.com>
2023-03-23 22:31:47 +0900

    add helper scripts

---
+++ src/pages/blog/img/202303-18kippu-picts/uncompressed/compress.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+TARGET="HEIC"
+
+while (( $# > 0 ))
+do
+  case $1 in
+    -heic)
+      TARGET="HEIC"
+      ;;
+    -png)
+      TARGET="png"
+      ;;
+    -*)
+      echo "invalid option"
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+echo "target:" $TARGET
+
+SCRIPT_DIR=$(dirname $0)
+cd $SCRIPT_DIR
+
+if [ $TARGET = "HEIC" ] ; then
+  mogrify -format png +dither -colors 256 -quality 75 -resize 1500x *.HEIC
+elif [ $TARGET = "png" ] ; then
+  mogrify +dither -colors 256 -quality 75 -resize 1500x *.png
+else
+  echo "ERROR : invalid target"
+  exit 1
+fi
+exit
+
