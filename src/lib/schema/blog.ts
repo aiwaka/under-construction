9bcdcd252081ba627abc313c689a721509504206
Aiwaka <Kw.I.KU.1414@gmail.com>
2023-04-07 01:23:01 +0900

    calc related articles

--- src/lib/schema/blog.ts
+++ src/lib/schema/blog.ts
@@ -16,6 +16,12 @@ export const BlogArticleSchema = z.object({
   date: z.date(),
   updateDate: z.date().optional(),
   tags: z.string().array(),
+  related: z
+    .string()
+    .array()
+    // 5個以上関連記事をセットできない
+    .refine((arg) => arg.length <= 5)
+    .default([]),
   latex: z.boolean().default(false),
   draft: z.boolean().default(false),
 });
