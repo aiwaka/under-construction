46946b5887577aba2ca80c0e53c8ef3022bb3281
Aiwaka <Kw.I.KU.1414@gmail.com>
2023-04-07 12:57:32 +0900

    move library

---
+++ src/lib/blog/image.ts
@@ -0,0 +1,10 @@
+/**
+ * `alt`, `caption`は両方省略はできず, この場合警告がコンソールに表示される.
+ * `alt`を省略した場合自動的に`caption`が用いられる.
+ */
+export interface BlogImageProps {
+  src: ImageMetadata | Promise<{ default: ImageMetadata }>;
+  width: number;
+  alt?: string;
+  caption?: string;
+}
--- src/lib/blog/index.ts
+++ src/lib/blog/index.ts
@@ -1,10 +1,5 @@
-/**
- * `alt`, `caption`は両方省略はできず, この場合警告がコンソールに表示される.
- * `alt`を省略した場合自動的に`caption`が用いられる.
- */
-export interface BlogImageProps {
-  src: ImageMetadata | Promise<{ default: ImageMetadata }>;
-  width: number;
-  alt?: string;
-  caption?: string;
-}
+import type { BlogImageProps } from "./image";
+import { getAllTagSet } from "./getTagSet";
+import { getBlogPostEntries } from "./getEntries";
+
+export { BlogImageProps, getAllTagSet, getBlogPostEntries };
