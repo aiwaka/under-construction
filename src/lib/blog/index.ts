553446bbcf8366e1cf93913e4ff32633f039ac3b
Aiwaka <Kw.I.KU.1414@gmail.com>
2023-04-08 18:25:22 +0900

    decompose method

--- src/lib/blog/index.ts
+++ src/lib/blog/index.ts
@@ -1,5 +1,6 @@
 import type { BlogImageProps } from "./image";
 import { getAllTagSet } from "./getTagSet";
 import { getBlogPostEntries } from "./getEntries";
+import { createRelated } from "./relatedPosts";
 
-export { BlogImageProps, getAllTagSet, getBlogPostEntries };
+export { BlogImageProps, getAllTagSet, getBlogPostEntries, createRelated };
