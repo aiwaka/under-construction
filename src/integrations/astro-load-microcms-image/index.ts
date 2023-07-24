670a417a7fbb027ca50282c038bc4b34789f73b0
Aiwaka <Kw.I.KU.1414@gmail.com>
2023-07-24 22:38:00 +0900

    fetch images data from microcms at build

--- src/integrations/astro-load-microcms-image/index.ts
+++ src/integrations/astro-load-microcms-image/index.ts
@@ -1,5 +1,6 @@
 import type { AstroIntegration } from "astro";
-import fs from "fs/promises";
+import fs from "fs";
+import { createClient } from "microcms-js-sdk";
 
 const PKG_NAME = "astro-load-microcms-image";
 
@@ -8,11 +9,31 @@ export default function preload(): AstroIntegration {
     name: PKG_NAME,
     hooks: {
       "astro:config:setup": async () => {
-        console.log("load-microcms-image")
-        // await fs.mkdir("dist/assets/preloaded", { recursive: true });
-        // const files = await fs.readdir("public/assets/preloaded");
-        // await Promise.all(files.map(async file => fs.copyFile(`public/assets/preloaded/${file}`, `dist/assets/preloaded/${file}`)));
-      }
-    }
+        (await import("dotenv")).config();
+        const MICROCMS_SERVICE_DOMAIN = process.env.MICROCMS_SERVICE_DOMAIN;
+        const MICROCMS_API_KEY = process.env.MICROCMS_API_KEY;
+        if (!(MICROCMS_SERVICE_DOMAIN && MICROCMS_API_KEY)) {
+          throw Error("MICROCMS_XXX_XXX variables are not defined in `.env`.");
+        }
+
+        const microCMSClient = createClient({
+          serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN!,
+          apiKey: process.env.MICROCMS_API_KEY!,
+        });
+        const imageDataFromMicroCMS = await microCMSClient.get({
+          endpoint: "images-in-articles",
+          queries: { fields: "id,title,thumbnail,images" },
+        });
+        const path = new URL(
+          "../../generated/images-data.json",
+          import.meta.url
+        );
+
+        fs.writeFileSync(
+          path,
+          JSON.stringify(imageDataFromMicroCMS["contents"])
+        );
+      },
+    },
   };
 }
