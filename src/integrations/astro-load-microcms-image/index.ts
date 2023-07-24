61bd3d93219600664fa14138bf7984cd367bd9df
Aiwaka <Kw.I.KU.1414@gmail.com>
2023-07-24 22:45:10 +0900

    data structure changed

--- src/integrations/astro-load-microcms-image/index.ts
+++ src/integrations/astro-load-microcms-image/index.ts
@@ -1,14 +1,60 @@
 import type { AstroIntegration } from "astro";
+import { z } from "astro/zod";
 import fs from "fs";
 import { createClient } from "microcms-js-sdk";
 
 const PKG_NAME = "astro-load-microcms-image";
 
-export default function preload(): AstroIntegration {
+interface LoadMicroCMSImageOptions {
+  skip?: boolean;
+}
+/** MicroCMSから取得する画像のスキーマ */
+const MicroCMSImageSchema = z.object({
+  url: z.string(),
+  width: z.number(),
+  height: z.number(),
+});
+/** microCMSから取得するブログ用画像コンテンツのスキーマ */
+const MicroCMSImagesDataSchema = z.array(
+  z.object({
+    id: z.string(),
+    title: z.string(),
+    thumbnail: MicroCMSImageSchema,
+    images: z.array(
+      z.object({
+        fieldId: z.literal("image"),
+        name: z.string(),
+        image: MicroCMSImageSchema,
+      })
+    ),
+  })
+);
+/** このブログプロジェクトで利用したい形式のスキーマ */
+interface ImagesStorageSchema {
+  [title: string]: {
+    thumbnail: z.infer<typeof MicroCMSImageSchema>;
+    images: {
+      [name: string]: z.infer<typeof MicroCMSImageSchema>;
+    };
+  };
+}
+
+export default function preload(
+  options: LoadMicroCMSImageOptions = {}
+): AstroIntegration {
+  const { skip = false } = options;
   return {
     name: PKG_NAME,
     hooks: {
-      "astro:config:setup": async () => {
+      "astro:config:setup": async ({ command, isRestart }) => {
+        if (skip) {
+          console.log("[load-microcms-image] fetch skipped.");
+          return;
+        }
+        if (isRestart) {
+          console.log("[load-microcms-image] fetch skipped when restarting.");
+          return;
+        }
         (await import("dotenv")).config();
         const MICROCMS_SERVICE_DOMAIN = process.env.MICROCMS_SERVICE_DOMAIN;
         const MICROCMS_API_KEY = process.env.MICROCMS_API_KEY;
@@ -17,8 +63,8 @@ export default function preload(): AstroIntegration {
         }
 
         const microCMSClient = createClient({
-          serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN!,
-          apiKey: process.env.MICROCMS_API_KEY!,
+          serviceDomain: MICROCMS_SERVICE_DOMAIN,
+          apiKey: MICROCMS_API_KEY,
         });
         const imageDataFromMicroCMS = await microCMSClient.get({
           endpoint: "images-in-articles",
@@ -28,11 +74,21 @@ export default function preload(): AstroIntegration {
           "../../generated/images-data.json",
           import.meta.url
         );
-
-        fs.writeFileSync(
-          path,
-          JSON.stringify(imageDataFromMicroCMS["contents"])
+        const contents = MicroCMSImagesDataSchema.parse(
+          imageDataFromMicroCMS["contents"]
         );
+        const resultContents: ImagesStorageSchema = {};
+        contents.forEach((content) => {
+          resultContents[content.title] = {
+            thumbnail: content.thumbnail,
+            images: Object.fromEntries(
+              content.images.map((image) => [image.name, image.image])
+            ),
+          };
+        });
+
+        fs.writeFileSync(path, JSON.stringify(resultContents));
+        console.log("[load-microcms-image] fetch and dump finished.");
       },
     },
   };
