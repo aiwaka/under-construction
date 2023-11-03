b653d450b0d7767fef40f74d5c0c96a4908011fa
aiwaka <kw.i.ku.1414@gmail.com>
2023-11-03 16:29:28 +0900

    improve thumbnail specifying

--- src/lib/schema/travelogue/collectionSchema.ts
+++ src/lib/schema/travelogue/collectionSchema.ts
@@ -6,28 +6,35 @@ export enum TravelogueThumbFormatEnum {
 }
 const TravelogueThumbFormatZodSchema = z.nativeEnum(TravelogueThumbFormatEnum);
 
-export const CollectionTravelogueZodSchema = z
-  .object({
-    title: z.string(),
-    description: z.string(),
-    // TODO: このサムネイル情報は使っていない. ブログのサムネイルから持ってこられると嬉しいのでなんとかする. その後はプロパティを削除する.
-    thumbnail: z.string(),
-    thumbnailFormat: TravelogueThumbFormatZodSchema.nullable().default(null),
-    // TODO: これらはpostsから自動で算出できるのが望ましい. blogのdateの仕様を旅行記に限り変更する.
-    startDate: z.date(),
-    endDate: z.date(),
-    posts: reference("blog").array(),
-    routes: reference("travelRoute"),
-  })
-  .refine(
-    ({ thumbnail, thumbnailFormat }) => {
-      return thumbnail === "remote" || thumbnailFormat !== null;
-    },
-    {
-      path: ["thumbnailFormat"],
-      message: "`thumbnail`が`remote`でない場合`thumbnailFormat`は必須です。",
-    },
-  );
+const TravelogueThumbZodSchema = z.union([
+  z.object({
+    type: z.literal("local"),
+    filename: z.string(),
+    format: TravelogueThumbFormatZodSchema,
+  }),
+  z.object({
+    type: z.literal("remote"),
+    id: z.string(),
+    name: z.string(),
+  }),
+  z.object({
+    type: z.literal("fromPost"),
+    id: z.string(),
+  }),
+]);
+
+export type TravelogueThumbSchema = z.infer<typeof TravelogueThumbZodSchema>;
+
+export const CollectionTravelogueZodSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  thumbnail: TravelogueThumbZodSchema,
+  // TODO: これらはpostsから自動で算出できるのが望ましい. blogのdateの仕様を旅行記に限り変更する.
+  startDate: z.date(),
+  endDate: z.date(),
+  posts: reference("blog").array(),
+  routes: reference("travelRoute"),
+});
 
 /**
  * 旅行記のfrontmatterのスキーマを表す型.
