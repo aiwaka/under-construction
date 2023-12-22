4c2e11dc3f0dc8eb824b4c44834a6a1102f01d54
aiwaka <kw.i.ku.1414@gmail.com>
2023-12-22 15:29:10 +0900

    - fix schema - add 'anchor' marker - design anchor in route node

---
+++ src/components/other/travelogue/MarkerIcon.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let color: string = "slateblue";
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 384 512"
+  style="width: 12px; height: 16px; opacity: 1;"
+  xml:space="preserve"
+>
+  <g>
+    <polygon
+      points="0.317,0 383.683,0 383.683,512 192,426 0.317,512 0.317,0"
+      style={`fill: ${color};`}
+    />
+  </g>
+</svg>
